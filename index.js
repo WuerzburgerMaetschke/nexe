9a3126a23c47a7867a7f00d71ebf4331ad80e889
Florian <f.maetschke@wuerzburger.com>
2025-11-19 11:46:22 +0100

    refactor: migrate index.js to ES module syntax and improve error handling

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
 #!/usr/bin/env node
-const options = require('./lib/options')
-if (require.main === module) {
+import { argv, help, version } from './lib/options.js'
+import { EOL } from 'os'
+import { compile } from './lib/nexe.js'
+import { NexeError } from './lib/compiler.js'
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const isMainModule = process.argv[1] === __filename
+
+if (isMainModule) {
   //fast path for help/version
-  const argv = options.argv
-  const eol = require('os').EOL
   const showHelp = argv.help || argv._.some(x => x === 'help')
   const showVersion = argv.version || argv._.some(x => x === 'version')
   if (showHelp || showVersion) {    
-    process.stderr.write(showHelp ? options.help : options.version + eol)
+    process.stderr.write(showHelp ? help : version + EOL)
   } else {
-    const nexe = require('./lib/nexe')
-    nexe.compile(argv).catch((error) => {
-      const NexeError = require('./lib/compiler').NexeError
-      const chalk = require('chalk')
+    compile(argv).catch((error) => {
       const isSilent = Boolean(argv.silent === true || argv.loglevel === 'silent')
       if (!isSilent) {
         if (error instanceof NexeError) {
-          process.stderr.write(eol + chalk.red('Error: ') + error.message + eol
-            + eol + 'See nexe -h for usage..' + eol + eol
+          process.stderr.write(EOL + chalk.red('Error: ') + error.message + EOL
+            + EOL + 'See nexe -h for usage..' + EOL + EOL
           )
         } else {
-          process.stderr.write(error.stack + eol)
+          process.stderr.write(error.stack + EOL)
         }
       }
 
       process.exit(1)
     })
   }
-} else {
-  module.exports = require('./lib/nexe')
 }
+
+export { compile }
+export default compile
